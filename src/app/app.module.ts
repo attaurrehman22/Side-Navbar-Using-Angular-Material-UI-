800262bca8b87ee771798fffc86610cc246abf80
Atta <attaurrehmanshah22@gmail.com>
2023-10-03 12:00:52 +0500

    Final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutComponent } from './components/about/about.component';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+const routes:Routes=[
+  {
+    path:"about",component:AboutComponent
+  }
+]
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
